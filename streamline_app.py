a27fea6e1da26e9eee4f0a80c0afd2bb23ce3cd7
Lusanda Shozi <110555086+MrShozi@users.noreply.github.com>
2023-10-20 10:45:01 +0200

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title("My first streamline app")
+
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
